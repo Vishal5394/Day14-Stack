d28a5828341f3da0044cf9655e8fbe5cb851cbd3
vishal5394 <vishaljadyal@gmail.com>
2022-07-30 18:37:09 +0530

    Added: Create Stack

--- StackNew.java
+++ StackNew.java
@@ -1,7 +1,55 @@
 package com.Bridgelabz.Day14;
 
-public class StackNew{
-	public static void main(String[] args) {
-		System.out.println("Welcome To stack Program");
-	}
+import static java.lang.System.exit;
+
+class StackUsingLinkedlist {
+    private class Node {
+        int data;
+        Node link; 
+    }
+  
+    Node top;
+    
+   StackUsingLinkedlist(){
+       this.top = null;
+    }
+  
+  public void push(int x) {
+        
+        Node temp = new Node();
+ 
+       if (temp == null) {
+            System.out.print("\n Heap Overflow");
+            return;
+        }
+        temp.data = x;
+        temp.link = top;
+        top = temp;
+    }
+    public void display() {
+        if (top == null) {
+            System.out.printf("\nStack Underflow");
+            exit(1);
+        }
+        else {
+            Node temp = top;
+            while (temp != null) {
+                System.out.printf("%d->", temp.data);
+  
+            temp = temp.link;
+            }
+        }
+    }
+}
+
+public class StackNew {
+    public static void main(String[] args){
+        
+        StackUsingLinkedlist obj = new StackUsingLinkedlist();
+        obj.push(70);
+        obj.push(30);
+        obj.push(56);
+        
+        obj.display();
+    }
 }
